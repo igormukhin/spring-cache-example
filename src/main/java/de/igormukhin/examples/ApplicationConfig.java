4db228069184444405c8a1f14da07a3390f9218a
Igor Mukhin <igor.mukhin@web-dienstleister.de>
2014-03-23 18:15:45 +0100

    SimpleCacheManager+GuavaCache

--- src/main/java/de/igormukhin/examples/ApplicationConfig.java
+++ src/main/java/de/igormukhin/examples/ApplicationConfig.java
@@ -3,13 +3,16 @@ package de.igormukhin.examples;
 import java.util.Arrays;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.guava.GuavaCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.cache.CacheBuilder;
 
 @Configuration
 @ComponentScan
@@ -19,9 +22,18 @@ public class ApplicationConfig {
 	
     @Bean
     public CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("dataCache")));
-        return cacheManager;
+    	// This standard implementation offered by spring-context-support
+    	// doesn't provide the opportunity to fine tune each cache
+    	// but creates caches on the fly which actually should be sufficient in many cases.
+    	//return new GuavaCacheManager();
+    	
+    	// configure the caches as you need it 
+    	Cache dataCache = new GuavaCache("dataCache", 
+    			CacheBuilder.newBuilder().maximumSize(1000).build());
+    	
+    	SimpleCacheManager manager = new SimpleCacheManager();
+    	manager.setCaches(Arrays.asList(dataCache));
+    	return manager;
     }
 
 }
